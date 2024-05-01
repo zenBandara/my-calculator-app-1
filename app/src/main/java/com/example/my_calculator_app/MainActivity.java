974599962f2cc07e70256678862be9bb1c0ae035
SLTDigitalLab <chathuramahasen@gmail.com>
2024-05-01 22:40:10 +0530

    intent added - goMul button

--- app/src/main/java/com/example/my_calculator_app/MainActivity.java
+++ app/src/main/java/com/example/my_calculator_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.my_calculator_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,7 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+        goMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+            }
+        });
 
     }
 }
